2597f28129d657ca2e44caa65631b7e7e90fbd38
mktosos <taniguchidesigns@hotmail.com>
2019-02-08 15:47:16 -0800

    htmlformatting and jquery added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,24 @@
 $(document).ready(function() {
-    var proxyJsonp="https://script.google.com/macros/s/AKfycbwmqG55tt2d2FcT_WQ3WjCSKmtyFpkOcdprSITn45-4UgVJnzp9/exec";
-jQuery.ajaxOrig=jQuery.ajax;jQuery.ajax=function(a,b){function d(a){a=encodeURI(a).replace(/&/g,"%26");return proxyJsonp+"?url="+a+"&callback=?"}var c="object"===typeof a?a:b||{};c.url=c.url||("string"===typeof a?a:"");var c=jQuery.ajaxSetup({},c),e=function(a,c){var b=document.createElement("a");b.href=a;return c.crossOrigin&&"http"==a.substr(0,4).toLowerCase()&&"localhost"!=b.hostname&&"127.0.0.1"!=b.hostname&&b.hostname!=window.location.hostname}(c.url,c);c.proxy&&0<c.proxy.length&&(proxyJsonp=c.proxy,"object"===typeof a?
-a.crossDomain=!0:"object"===typeof b&&(b.crossDomain=!0));e&&("object"===typeof a?a.url&&(a.url=d(a.url),a.charset&&(a.url+="&charset="+a.charset),a.dataType="json"):"string"===typeof a&&"object"===typeof b&&(a=d(a),b.charset&&(a+="&charset="+b.charset),b.dataType="json"));return jQuery.ajaxOrig.apply(this,arguments)};jQuery.ajax.prototype=new jQuery.ajaxOrig;jQuery.ajax.prototype.constructor=jQuery.ajax;
-
-    baseUrl = "https://www.target.com/s?searchTerm=014633734096";
-    $.ajax({
+  var session =[];
+  var input = document.getElementById("iBar");
+  
+  function runAjax(baseUrl){
+    return $.ajax({
       url: baseUrl,
-      crossOrigin: true,
-      type: "get",
-      dataType: "text",
-      success: function(data) {
-        console.log(data);
-      }
+      method: "GET",
+    }).then(function(res){
+      console.log("res", res.items[0].images);
+      console.log(baseUrl);
     });
-    
+  }
+
+  input.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13 && document.getElementById("iBar").value!=="") {
+      var upc = document.getElementById("iBar").value;
+      baseUrl = "https://api.upcitemdb.com/prod/trial/lookup?upc="+ upc;
+      setTimeout(function(){runAjax(baseUrl)}, 500);
+    };
   });
+    
+});

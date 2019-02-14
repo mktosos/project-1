f5c92698d7d07a29d1c7eb55a763a9727fd90bda
mktosos <taniguchidesigns@hotmail.com>
2019-02-14 14:19:30 -0800

    README updated, time added to history

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,94 +11,85 @@ $(document).ready(function() {
   firebase.initializeApp(config);
   var database = firebase.database();
   
- 
-  //grab data from database on value change or on page start to display
+  //grab data from firebase database on value change or on page start to display
   database.ref().on('child_added',function(snapshot) {
     console.log(snapshot.val());
-    //jqeury the values as text to ID div
+    //$ display time/title/upc in recent searches div
     var $item = $('<div>');
-    $item.append('<div>'+ snapshot.val().resultObj.items[0].title + " UPC:"+ snapshot.val().resultObj.items[0].upc+'</div>');
-
+    $item.append('<div>'+ snapshot.val().date + " : "+ snapshot.val().resultObj.items[0].title + " UPC:"+ snapshot.val().resultObj.items[0].upc+'</div>');
     $('#history').append($item);
-    // $('#title').text(snapshot.val().title);
-    // $('#location').text(snapshot.val().location.lat);
-    // $('#upc').text(snapshot.val().upc);
     
     }, function(errorObject) {
-
     // In case of error this will print the error
     console.log("The read failed: " + errorObject.code);
   });
 
-  
   //search history array
   var session =[];
   var input = document.getElementById("iBar");
   
   input.addEventListener("keyup", function(event) {
     event.preventDefault();
-
     //listens for return key
     if (event.keyCode === 13 && input.value!=="") {
-     
       mainFunction(input.value, session);
     };
   });
 
 function mainFunction(value, session){
-  
-      //UPC code validation
-      if (isNaN(value) || value.length !=12) {
-        text = "Input not valid";
-        console.log("not valid");
-        document.getElementById("iBar").value=""
-        document.getElementById("iBar").value="Requires a 12-digit UPC code...try again"
-        setTimeout(function(){document.getElementById("iBar").value=""}, 1750);
-      } else {
-        //push UPC search to history and add UPC to ajax url to be called
-        //console.log(event.keyCode);
-        var upc = document.getElementById("iBar").value;
-        session.unshift(upc);
-        var proxy = "https://cors-anywhere.herokuapp.com/";
-        baseUrl = proxy + "https://api.upcitemdb.com/prod/trial/lookup?upc="+ upc;
-        runAjax(baseUrl);
+  //UPC code validation
+  if (isNaN(value) || value.length !=12) {
+    text = "Input not valid";
+    console.log("not valid");
+    document.getElementById("iBar").value=""
+    document.getElementById("iBar").value="Requires a 12-digit UPC code...try again"
+    setTimeout(function(){document.getElementById("iBar").value=""}, 1750);
+  } else {
+    //push UPC search to history and add UPC to ajax url to be called
+    //console.log(event.keyCode);
+    var upc = document.getElementById("iBar").value;
+    session.unshift(upc);
+    var proxy = "https://cors-anywhere.herokuapp.com/";
+    baseUrl = proxy + "https://api.upcitemdb.com/prod/trial/lookup?upc="+ upc;
+    runAjax(baseUrl);
 
-        //ajax function
-        function runAjax(baseUrl){
-          return $.ajax({
-            url: baseUrl,
-            method: "GET",
-          }).then(function(res){
-            $('.offers').text("");
-            console.log(res);
-            //push ajax resultObj to firebase
-            setTimeout(function(){ database.ref().push({
-              resultObj: res,
-              location: pos
-              });
-            }, 5000);
-            
-            //clear start screen populate offers with store/link and product images
-            for (let i = 0; i < res.items[0].offers.length; i++) {
-              var a =res.items[0].offers[i].link
-              if(res.items[0].images[i] !== undefined){
-              $('.startText').text("");
-              $('#startText1').text(res.items[0].offers[0].title);
-              $('#startText2').text("UPC: "+upc);
-              $('#startText3').text("historical price range: $"+res.items[0].highest_recorded_price+" / $"+res.items[0].lowest_recorded_price);
-              $('.offers').prepend("<tr><td><a href="+a+ " target='_blank'>"+res.items[0].offers[i].merchant+"</a></td><td><a href="+a+ " target='_blank'> $"+res.items[0].offers[i].price+"</a></td></tr>");  
-              $('.images').prepend("<a href="+a+" target='_blank'><img src="+res.items[0].images[i]+" height='70' alt=''></a>"); 
-              }
-            }
-            console.log("res", res.items[0].offers[0].title);
-            //populate history of upc searches list/field
-            //$('#history').prepend("<li class='history'><a href='#' target='_blank'>"+upc+': '+res.items[0].offers[0].title+"</a></li>");
-            //clear input field after 1sec
-            setTimeout(function(){document.getElementById("iBar").value=""}, 1000);
-            });
+    //ajax function
+  function runAjax(baseUrl){
+    return $.ajax({
+      url: baseUrl,
+      method: "GET",
+    }).then(function(res){
+      $('.offers').text("");
+      console.log(res);
+      var date =moment().format('MMMM Do YYYY, h:mm:ss a');
+      //push ajax resultObj to firebase
+      setTimeout(function(){ database.ref().push({
+        resultObj: res,
+        location: pos,
+        date: date
+        });
+      }, 5000);
+      
+      //clear start screen populate offers with store/link and product images
+      for (let i = 0; i < res.items[0].offers.length; i++) {
+        var a =res.items[0].offers[i].link
+        if(res.items[0].images[i] !== undefined){
+        $('.startText').text("");
+        $('#startText1').text(res.items[0].offers[0].title);
+        $('#startText2').text("UPC: "+upc);
+        $('#startText3').text("historical price range: $"+res.items[0].highest_recorded_price+" / $"+res.items[0].lowest_recorded_price);
+        $('.offers').prepend("<tr><td><a href="+a+ " target='_blank'>"+res.items[0].offers[i].merchant+"</a></td><td><a href="+a+ " target='_blank'> $"+res.items[0].offers[i].price+"</a></td></tr>");  
+        $('.images').append("<a href="+a+" target='_blank'><img src="+res.items[0].images[i]+" height='70' alt=''></a>"); 
         }
-        console.log(session);
-        console.log(pos);
       }
+      console.log("res", res.items[0].offers[0].title);
+    
+      //clear input field after 1sec
+      setTimeout(function(){document.getElementById("iBar").value=""}, 1000);
+      });
+    }
+    console.log(session);
+    console.log(pos);
+  }
 }
 });
